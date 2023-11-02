2aa929a683c3567d2672a14f62e9d7bebdfbd4ba
Tianpai Z <tianpai.zhang@mail.utoronto.ca>
2023-11-02 02:06:50 -0400

    bruh, another rewrite

--- setup.sh
+++ setup.sh
@@ -1,81 +1,75 @@
 #!/bin/bash
 
-# Function to get the distribution name
-get_distro() {
-  if [ -f /etc/os-release ]; then
-    . /etc/os-release
-    echo $ID
-  elif type lsb_release >/dev/null 2>&1; then
-    lsb_release -si | tr 'A-Z' 'a-z'
+# Function to check if a command exists
+command_exists() {
+  which "$1" &>/dev/null
+}
+
+# Function to install curl
+install_curl() {
+  if [[ -f /etc/fedora-release ]]; then
+    sudo dnf install -y curl
+  elif [[ -f /etc/arch-release ]]; then
+    sudo pacman -S --noconfirm curl
+  elif [[ -f /etc/lsb-release ]]; then
+    sudo apt-get update
+    sudo apt-get install -y curl
   else
-    echo "unknown"
+    echo "Unknown distribution. Exiting..."
+    exit 1
   fi
 }
 
-# Function to check and install python3
-check_install_python3() {
-  if ! which python3 >/dev/null 2>&1; then
-    echo "Python3 could not be found, installing..."
-    case $1 in
-      ubuntu|debian)
-        sudo apt-get update
-        sudo apt-get install -y python3
-        ;;
-      fedora)
-        sudo dnf install -y python3
-        ;;
-      arch|manjaro)
-        sudo pacman -S --noconfirm python
-        ;;
-      *)
-        echo "Unsupported or unknown distribution"
-        exit 1
-        ;;
-    esac
-  else
-    echo "Python3 is already installed"
+# Function to install Homebrew
+install_homebrew() {
+  if ! command_exists brew; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   fi
 }
 
-# Function to check and install curl
-check_install_curl() {
-  if ! which curl >/dev/null 2>&1; then
-    echo "Curl could not be found, installing..."
-    case $1 in
-      ubuntu|debian)
-        sudo apt-get update
-        sudo apt-get install -y curl
-        ;;
-      fedora)
-        sudo dnf install -y curl
-        ;;
-      arch|manjaro)
-        sudo pacman -S --noconfirm curl
-        ;;
-      *)
-        echo "Unsupported or unknown distribution"
-        exit 1
-        ;;
-    esac
-  else
-    echo "Curl is already installed"
-  fi
+# Function to install Node and Python3 using Homebrew
+install_node_python3() {
+  brew install node python3
 }
 
-# Get the distribution name
-distro=$(get_distro)
+# Function to install Packer.vim and plugins
+install_packer_plugins() {
+  if ! command_exists nvim; then
+    echo "Neovim is not installed. Please install Neovim first."
+    exit 1
+  fi
 
-# Check and install python3
-check_install_python3 $distro
+  # Check Neovim version
+  NVIM_VERSION=$(nvim --version | head -n 1 | cut -d " " -f 2)
+  if [[ $(echo -e "0.5.0\n$NVIM_VERSION" | sort -V | head -n1) = "0.5.0" ]]; then
+    echo "Neovim version is sufficient for Packer.vim"
+  else
+    brew install neovim
+  fi
 
-# Check and install curl
-check_install_curl $distro
+  # Install Packer.vim
+  PACKER_PATH="$HOME/.local/share/nvim/site/pack/packer/start/packer.nvim"
+  if [[ ! -d "$PACKER_PATH" ]]; then
+    git clone --depth 1 https://github.com/wbthomason/packer.nvim "$PACKER_PATH"
+  fi
 
-# Run the Python script
-if [ -f setup.py ]; then
-  python3 setup.py
-else
-  echo "The Python script 'setup.py' was not found in the current directory."
-  exit 1
-fi
+  # Compile Packer and install plugins
+  nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
+}
+
+# Main script
+main() {
+  if ! command_exists curl; then
+    echo "Curl is not installed. Installing..."
+    install_curl
+  else
+    echo "Curl is already installed."
+  fi
+
+  install_homebrew
+  install_node_python3
+  install_packer_plugins
+}
+
+main
 
