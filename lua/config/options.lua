61fabdead0fef3b4bee4d1742b4c2cf5cace3bf6
Tianpai Z <tianpai.zhang@mail.utoronto.ca>
2023-11-29 20:21:29 -0500

    swtich to lazyvim

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,5 +1,9 @@
-local opt = vim.opt
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+--
 
+local opt = vim.opt
 -- line number
 opt.relativenumber = true
 opt.number = true
