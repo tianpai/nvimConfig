238e5f6361350a3ca00856b5d8e6ec4fec6ea8df
Tianpai Z <tianpai.zhang@mail.utoronto.ca>
2023-12-11 14:31:50 -0500

    update

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,12 @@
+return {
+  "williamboman/mason.nvim",
+  opt = {
+    ui = {
+      icons = {
+        package_installed = "I",
+        package_pending = "➜",
+        package_uninstalled = "✗",
+      },
+    },
+  },
+}
