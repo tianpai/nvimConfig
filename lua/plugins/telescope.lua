10d8433440d7f4afa8fa81c50102936b9fcd87a3
Tianpai Z <tianpai.zhang@mail.utoronto.ca>
2023-12-23 05:54:11 -0500

    experimental minimal config with lazy

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,25 +1,14 @@
--- change some telescope options and a keymap to browse plugin files
-return {
-  "nvim-telescope/telescope.nvim",
-  -- change some options
-  opts = {
-    defaults = {
-      mappings = {
-        i = {
-          ["<esc>"] = require("telescope.actions").close,
-          ["<C-j>"] = require("telescope.actions").move_selection_next,
-          ["<C-k>"] = require("telescope.actions").move_selection_previous,
-        },
-      },
-      -- vertical view
-      layout_strategy = "vertical",
-      layout_config = {
-        width = vim.o.columns,
-        height = vim.o.lines,
-        prompt_position = "bottom",
-        preview_height = 0.6,
-      },
-      preview = { filesize_limit = 0.3 }, -- MB
-    },
-  },
-}
+return 
+    {
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.5',
+        dependencies = 
+            { 
+                'nvim-lua/plenary.nvim' 
+            },
+        config = function()
+            local builtin = require("telescope.builtin")
+            vim.keymap.set('n', '<leader><leader>', builtin.find_files, {})
+            vim.keymap.set('n', '<leader>g', builtin.live_grep, {})
+        end
+    }
