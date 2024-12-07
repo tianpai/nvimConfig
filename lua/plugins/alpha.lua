70d64e594d94a8def962d5e700b4dc8a06a35825
Tianpai Z. <tianpai.zhang@alumni.utoronto.ca>
2024-12-07 16:12:40 -0500

    remove extra options

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -15,12 +15,12 @@ return {
     -- stylua: ignore
     dashboard.section.buttons.val = {
       dashboard.button("f", " " .. " Find file",       LazyVim.pick()),
-      dashboard.button("n", " " .. " New file",        [[<cmd> ene <BAR> startinsert <cr>]]),
+      -- dashboard.button("n", " " .. " New file",        [[<cmd> ene <BAR> startinsert <cr>]]),
       dashboard.button("r", " " .. " Recent files",    LazyVim.pick("oldfiles")),
-      dashboard.button("g", " " .. " Find text",       LazyVim.pick("live_grep")),
+      -- dashboard.button("g", " " .. " Find text",       LazyVim.pick("live_grep")),
       dashboard.button("c", " " .. " Config",          LazyVim.pick.config_files()),
-      dashboard.button("s", " " .. " Restore Session", [[<cmd> lua require("persistence").load() <cr>]]),
-      dashboard.button("x", " " .. " Lazy Extras",     "<cmd> LazyExtras <cr>"),
+      -- dashboard.button("s", " " .. " Restore Session", [[<cmd> lua require("persistence").load() <cr>]]),
+      -- dashboard.button("x", " " .. " Lazy Extras",     "<cmd> LazyExtras <cr>"),
       dashboard.button("l", "󰒲 " .. " Lazy",            "<cmd> Lazy <cr>"),
       dashboard.button("q", " " .. " Quit",            "<cmd> qa <cr>"),
     }
