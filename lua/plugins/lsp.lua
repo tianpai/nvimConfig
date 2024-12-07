ac3142a3120d28c9eef2e080630d9e2fc5343abe
Tianpai Z. <tianpai.zhang@alumni.utoronto.ca>
2024-12-07 16:14:09 -0500

    why rust has to be different?

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,48 +1,17 @@
-local lspconfig = require("lspconfig")
-
-lspconfig.rust_analyzer.setup({
-  on_attach = function(client, bufnr)
-    vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
-  end,
-  settings = {
-    ["rust-analyzer"] = {
-      imports = {
-        granularity = {
-          group = "module",
-        },
-        prefix = "self",
-      },
-      cargo = {
-        buildScripts = {
-          enable = true,
-        },
-      },
-      procMacro = {
-        enable = true,
-      },
-    },
-  },
-})
-
 return {
   "neovim/nvim-lspconfig",
   opts = {
-    servers = {
-      -- rust_analyzer = false,
-    },
+    -- servers = {
+    --   rust_analyzer = false,
+    -- },
     diagnostics = {
       underline = true,
       update_in_insert = false,
-      -- virtual_text = {
-      --   spacing = 4,
-      --   source = "if_many",
-      --   prefix = "●",
-      -- },
       virtual_text = false,
     },
     inlay_hints = {
       enabled = true,
-      exclude = { "javascript" }, -- filetypes for which you don't want to enable inlay hints
+      exclude = { "javascript", "python" }, -- filetypes for which you don't want to enable inlay hints
     },
   },
 }
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,15 +1,15 @@
 return {
-  "williamboman/mason.nvim",
-  opt = {
-    servers = {
-      -- rust_analyzer = true,
-    },
-    ui = {
-      icons = {
-        package_installed = "I",
-        package_pending = "➜",
-        package_uninstalled = "✗",
-      },
-    },
-  },
+  -- "williamboman/mason.nvim",
+  -- opt = {
+  --   -- servers = {
+  --   --   -- rust_analyzer = true,
+  --   -- },
+  --   ui = {
+  --     icons = {
+  --       package_installed = "I",
+  --       package_pending = "➜",
+  --       package_uninstalled = "✗",
+  --     },
+  --   },
+  -- },
 }
