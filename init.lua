44ff99cb247725acad229fe8303a1979acc93b54
Tianpai Z. <tianpai.zhang@alumni.utoronto.ca>
2024-12-07 16:11:56 -0500

    neovide on large display

--- init.lua
+++ init.lua
@@ -1,2 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+if vim.g.neovide then
+  -- Put anything you want to happen only in Neovide here
+  vim.g.neovide_scale_factor = 1.5
+end
